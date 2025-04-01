61019a1ae4b814021f4e4b60b1133865d91fcb36
Gunvansh Siingh <vansh.bhatia9@gmail.com>
2025-03-31 19:12:43 -0700

    Added FE tests

--- leaderboard-frontend/src/App.test.js
+++ leaderboard-frontend/src/App.test.js
@@ -1,8 +1,41 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+// import { render, screen,fireEvent } from '@testing-library/react';
+// import UserList from '../src/components/UserList';
+// import { LeaderboardProvider } from '../src/contexts/LeaderBoardContext';
+// import AddUserForm from '../src/components/AddUserForm';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
+
+
+// jest.mock('axios');
+
+// const mockUsers = [
+//   { id: 1, name: 'Test User', points: 10 }
+// ];
+
+// test('renders user list', () => {
+//   render(
+//     <LeaderboardProvider value={{ users: mockUsers }}>
+//       <UserList />
+//     </LeaderboardProvider>
+//   );
+
+//   expect(screen.getByText('Test User')).toBeInTheDocument();
+//   expect(screen.getByText('10')).toBeInTheDocument();
+// });
+
+
+// test('submits form data', () => {
+//   const mockAddUser = jest.fn();
+  
+//   render(<AddUserForm addUser={mockAddUser} />);
+  
+//   fireEvent.change(screen.getByLabelText(/name/i), { 
+//     target: { value: 'New User' } 
+//   });
+//   fireEvent.click(screen.getByText(/add user/i));
+  
+//   expect(mockAddUser).toHaveBeenCalledWith({
+//     name: 'New User',
+//     age: '',
+//     address: ''
+//   });
 // });
